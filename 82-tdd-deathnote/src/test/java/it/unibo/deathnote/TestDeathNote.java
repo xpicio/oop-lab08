d3295d8668313cf3f95876096e5e41d926da353f
Patrizio Bertozzi <patrizio.bertozzi@studio.unibo.it>
2024-11-11 17:48:55 +0100

    test(82-tdd-deathnote): refactor tests following PMD suggestions

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -15,15 +15,15 @@ import it.unibo.deathnote.impl.DeathNoteImplementation;
 import it.unibo.deathnote.api.DeathNote;
 
 class TestDeathNote {
-    final private static int INVALID_MIN_RULES_INDEX = -5;
-    final private static int INVALID_MAX_RULES_INDEX = 14;
-    final private static String DEFAULT_CAUSE_OF_DEATH = "heart attack";
-    final private static String HUMAN_NAME = "Mario";
-    final private static String HUMAN_NAME_02 = "Lucia";
+    private static final int INVALID_MIN_RULES_INDEX = -5;
+    private static final int INVALID_MAX_RULES_INDEX = 14;
+    private static final String DEFAULT_CAUSE_OF_DEATH = "heart attack";
+    private static final String HUMAN_NAME = "Mario";
+    private static final String HUMAN_NAME_02 = "Lucia";
     private DeathNote deathNote;
 
     @BeforeEach
-    public void init() {
+    void init() {
         this.deathNote = new DeathNoteImplementation();
     }
 
@@ -32,7 +32,7 @@ class TestDeathNote {
      */
     @ParameterizedTest(name = "Rule number {0}")
     @ValueSource(ints = { 0, INVALID_MIN_RULES_INDEX, INVALID_MAX_RULES_INDEX })
-    public void ruleShouldNotExist(int ruleNumber) {
+    void ruleShouldNotExist(final int ruleNumber) {
         final Exception illegalArgumentException = assertThrows(
                 IllegalArgumentException.class,
                 () -> this.deathNote.getRule(ruleNumber));
@@ -47,7 +47,7 @@ class TestDeathNote {
      * [2]
      */
     @Test
-    public void ruleShouldNotBeNullOrBlankOrEmpty() {
+    void ruleShouldNotBeNullOrBlankOrEmpty() {
         for (final String rule : DeathNote.RULES) {
             assertNotNull(rule);
             assertFalse(rule.isBlank());
@@ -59,7 +59,7 @@ class TestDeathNote {
      * [3]
      */
     @Test
-    public void shouldWriteName() {
+    void shouldWriteName() {
         // check the Mario will be write to the DeathNote
         assertFalse(this.deathNote.isNameWritten(HUMAN_NAME));
         this.deathNote.writeName(HUMAN_NAME);
@@ -67,20 +67,22 @@ class TestDeathNote {
     }
 
     @Test
-    public void shouldThrowExceptionWhenWritingNullName() {
+    void shouldThrowExceptionWhenWritingNullName() {
         // null name should throw exception
         assertThrows(
                 NullPointerException.class,
                 () -> this.deathNote.writeName(null));
     }
 
-    public void shouldRejectBlankName() {
+    @Test
+    void shouldRejectBlankName() {
         final String blankHumanName = "  ";
         this.deathNote.writeName(blankHumanName);
         assertFalse(this.deathNote.isNameWritten(blankHumanName));
     }
 
-    public void shouldRejectEmptyName() {
+    @Test
+    void shouldRejectEmptyName() {
         final String emptyHumanName = "";
         this.deathNote.writeName(emptyHumanName);
         assertFalse(this.deathNote.isNameWritten(emptyHumanName));
@@ -90,7 +92,7 @@ class TestDeathNote {
      * [4]
      */
     @Test
-    public void shouldThrowExceptionWhenWritingCauseOfDeath() {
+    void shouldThrowExceptionWhenWritingCauseOfDeath() {
         // null cause and missing human name should throw exception
         assertThrows(
                 IllegalStateException.class,
@@ -101,13 +103,13 @@ class TestDeathNote {
     }
 
     @Test
-    public void shouldDefaultCauseOfDeathBeHeartAttack() {
+    void shouldDefaultCauseOfDeathBeHeartAttack() {
         this.deathNote.writeName(HUMAN_NAME);
         assertEquals(DEFAULT_CAUSE_OF_DEATH, this.deathNote.getDeathCause(HUMAN_NAME));
     }
 
     @Test
-    public void shouldWriteCauseOfDeath() {
+    void shouldWriteCauseOfDeath() {
         final String causeOfDeath = "karting accident";
 
         this.deathNote.writeName(HUMAN_NAME);
@@ -116,7 +118,8 @@ class TestDeathNote {
     }
 
     @Test
-    public void shouldCauseOfDeathBeUnmodifiable() throws InterruptedException {
+    void shouldCauseOfDeathBeUnmodifiable() throws InterruptedException {
+        final int millisecondsWaitingForWriteFail = 100;
         final String causeOfDeath = "karting accident";
 
         // add first human
@@ -124,7 +127,7 @@ class TestDeathNote {
         this.deathNote.writeDeathCause(causeOfDeath);
         // add new human waiting too much
         this.deathNote.writeName(HUMAN_NAME_02);
-        Thread.sleep(100);
+        Thread.sleep(millisecondsWaitingForWriteFail);
         assertFalse(this.deathNote.writeDeathCause(causeOfDeath));
         assertEquals(DEFAULT_CAUSE_OF_DEATH, this.deathNote.getDeathCause(HUMAN_NAME_02));
     }
@@ -133,7 +136,7 @@ class TestDeathNote {
      * [5]
      */
     @Test
-    public void shouldThrowExceptionWhenWritingDetailsOfDeath() {
+    void shouldThrowExceptionWhenWritingDetailsOfDeath() {
         // null details and missing human name should throw exception
         assertThrows(
                 IllegalStateException.class,
@@ -144,7 +147,7 @@ class TestDeathNote {
     }
 
     @Test
-    public void shouldWriteDetailsOfDeath() {
+    void shouldWriteDetailsOfDeath() {
         final String detailsOfDeath = "ran for too long";
 
         this.deathNote.writeName(HUMAN_NAME);
@@ -153,7 +156,8 @@ class TestDeathNote {
     }
 
     @Test
-    public void shouldDetailsOfDeathBeUnmodifiable() throws InterruptedException {
+    void shouldDetailsOfDeathBeUnmodifiable() throws InterruptedException {
+        final int millisecondsWaitingForWriteFail = 6100;
         final String detailsOfDeath = "ran for too long";
 
         // add first human
@@ -161,7 +165,7 @@ class TestDeathNote {
         this.deathNote.writeDetails(detailsOfDeath);
         // add new human waiting too much
         this.deathNote.writeName(HUMAN_NAME_02);
-        Thread.sleep(6100);
+        Thread.sleep(millisecondsWaitingForWriteFail);
         assertFalse(this.deathNote.writeDetails(detailsOfDeath));
         assertEquals("", this.deathNote.getDeathDetails(HUMAN_NAME_02));
     }
